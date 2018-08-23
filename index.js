3858e00032c7ad1973f0765582d83f0c006fd1ed
Matthias Jenny <matthiascjenny@gmail.com>
2018-08-22 21:05:37 -0400

    Prettify output

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const {
   parentPort,
   workerData,
 } = require('worker_threads');
+const chalk = require('chalk');
+const ora = require('ora');
 
 const syncFib = require('./fib_functions/sync.js');
 const syncBusyFib = require('./fib_functions/sync_busy.js');
@@ -12,23 +14,25 @@ const asyncFib = require('./fib_functions/async.js');
 const asyncBusyFib = require('./fib_functions/async_busy.js');
 const asyncMemoFib = require('./fib_functions/async_memo.js');
 const displayResult = require('./util/display_result.js');
-const executeWithWorker = require('./workers/execute_with_worker.js');
-
-const fourthArg = process.argv[4];
-const noWorker = ['-nw', '--mode=no-worker'].includes(fourthArg);
-const withWorker = !fourthArg || ['-w', '--mode=worker'].includes(fourthArg);
-
-const fcnStr = process.argv[2];
-const fcnStrs = [
-  'syncFib',
-  'syncBusyFib',
-  'syncMemoFib',
-  'asyncFib',
-  'asyncBusyFib',
-  'asyncMemoFib',
-];
 
 if (isMainThread) {
+  const fourthArg = process.argv[4];
+  const noWorker = ['-nw', '--mode=no-worker'].includes(fourthArg);
+  const withWorker = !fourthArg || ['-w', '--mode=worker'].includes(fourthArg);
+
+  const fcnStr = process.argv[2];
+  const fcnStrs = [
+    'syncFib',
+    'syncBusyFib',
+    'syncMemoFib',
+    'asyncFib',
+    'asyncBusyFib',
+    'asyncMemoFib',
+  ];
+
+  const spinner = ora('Calculating');
+  spinner.color = 'green';
+
   if (fcnStrs.includes(fcnStr)) {
     const fcn = eval(fcnStr);
     const nStr = parseInt(process.argv[3]);
@@ -36,42 +40,51 @@ if (isMainThread) {
     if (noWorker) {
       if (n > 0) {
         if (fcnStr[0] === 's') {
+          spinner.start();
           const beforeTime = new Date().getTime();
           const result = fcn(n);
           const afterTime = new Date().getTime();
           const duration = afterTime - beforeTime;
-
+          spinner.stop(true);
           displayResult(n, duration, result);
         } else {
+          spinner.start();
           const beforeTime = new Date().getTime();
           fcn(n).then(function(result) {
             const afterTime = new Date().getTime();
             const duration = afterTime - beforeTime;
-
+            spinner.stop(true);
             displayResult(n, duration, result);
           });
         }
       } else {
-        console.log(
-          'The second arguments needs to be an integer greater than 0.' +
-            ` You entered: ${nStr}.`
+        console.error(
+          chalk.red(
+            'The second arguments needs to be an integer greater than 0.' +
+              ` You entered: ${nStr}.`
+          )
         );
       }
     } else if (withWorker) {
+      spinner.start();
+      // spinner.setSpinnerString('|/-\\');
       worker = new Worker(__filename, {
         workerData: { n, fcnStr },
       });
       worker.on('message', function(msg) {
+        spinner.stop(true);
         displayResult(n, msg.duration, msg.result);
       });
     } else {
-      console.log(`Invalid option: ${fourthArg}`);
+      console.error(chalk.red(`Invalid option: ${fourthArg}`));
     }
   } else {
-    console.log(
-      `The first argument needs to be one of: ${fcnStrs.join(
-        ', '
-      )}. You entered: ${fcnStr}.`
+    console.error(
+      chalk.red(
+        `The first argument needs to be one of: ${fcnStrs.join(
+          ', '
+        )}. You entered: ${fcnStr}.`
+      )
     );
   }
 } else {
--- util/display_result.js
+++ util/display_result.js
@@ -1,7 +1,15 @@
+const chalk = require('chalk');
+
 const nth = require('./nth.js');
 const stringify = require('./stringify.js');
 
 module.exports = function(n, duration, result) {
-  console.log(`The ${nth(n)} Fibonacci number is ${stringify(result)}.`);
-  console.log(`It took ${stringify(duration)} milliseconds to calculate this.`);
+  console.log(
+    `The ${nth(n)} Fibonacci number is ${chalk.bold.yellow(stringify(result))}.`
+  );
+  console.log(
+    `It took ${chalk.bold.cyan(
+      stringify(duration) + ' milliseconds'
+    )} to calculate this.`
+  );
 };
